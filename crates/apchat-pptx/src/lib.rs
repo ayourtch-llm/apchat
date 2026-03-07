131014378e8522fbae95266b7c8b6e9b35a4a1af
Andrew Yourtchenko <ayourtch@gmail.com>
2026-03-07 16:49:47 +0100

    feat: implement PPTX save with ZIP packaging
    Change-Id: Iac6ad1c378b1ea811f703fb8c52f45be4a094c07

--- crates/apchat-pptx/src/lib.rs
+++ crates/apchat-pptx/src/lib.rs
@@ -3,6 +3,11 @@ pub mod xml;
 
 pub use error::{PptxError, Result};
 
+use std::fs::File;
+use std::io::{Cursor, Write};
+use zip::write::FileOptions;
+use zip::ZipWriter;
+
 pub struct Presentation {
     slides: Vec<Slide>,
     title: Option<String>,
@@ -61,6 +66,121 @@ impl Presentation {
             title: Some(title.to_string()),
             content: bullets.into_iter().map(|s| s.to_string()).collect(),
         });
+    }
+    
+    pub fn save(&self, path: &str) -> Result<()> {
+        let mut file = File::create(path)?;
+        let mut zip = ZipWriter::new(Cursor::new(Vec::new()));
+        
+        let opts = FileOptions::default()
+            .compression_method(zip::CompressionMethod::Stored);
+        
+        // Add required PPTX files
+        self.add_presentation_xml(&mut zip, opts)?;
+        self.add_slides_xml(&mut zip, opts)?;
+        self.add_content_types(&mut zip, opts)?;
+        self.add_core_properties(&mut zip, opts)?;
+        self.add_app_properties(&mut zip, opts)?;
+        
+        let cursor = zip.finish()?;
+        let data = cursor.into_inner();
+        std::io::Write::write_all(&mut file, &data)?;
+        
+        Ok(())
+    }
+    
+    fn add_presentation_xml(&self, zip: &mut ZipWriter<Cursor<Vec<u8>>>, opts: FileOptions) -> Result<()> {
+        let xml = xml::generate_presentation_xml(self.slides.len());
+        zip.start_file("ppt/presentation.xml", opts)?;
+        zip.write_all(xml.as_bytes())?;
+        Ok(())
+    }
+    
+    fn add_slides_xml(&self, zip: &mut ZipWriter<Cursor<Vec<u8>>>, opts: FileOptions) -> Result<()> {
+        for (i, slide) in self.slides.iter().enumerate() {
+            let xml = xml::generate_slide_xml(
+                i + 1,
+                slide.title.as_deref(),
+                &slide.content,
+                match slide.slide_type {
+                    SlideType::Title => "title",
+                    SlideType::Content => "content",
+                }
+            );
+            zip.start_file(format!("ppt/slides/slide{}.xml", i + 1), opts)?;
+            zip.write_all(xml.as_bytes())?;
+        }
+        Ok(())
+    }
+    
+    fn add_content_types(&self, zip: &mut ZipWriter<Cursor<Vec<u8>>>, opts: FileOptions) -> Result<()> {
+        let xml = format!(
+            r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+  <Default Extension="xml" ContentType="application/xml"/>
+  <Override PartName="/ppt/presentation.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml"/>
+  {}
+</Types>"#,
+            (1..=self.slides.len())
+                .map(|i| format!("  <Override PartName=\"/ppt/slides/slide{}.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.presentationml.slide+xml\"/>", i))
+                .collect::<Vec<_>>()
+                .join("\n")
+        );
+        zip.start_file("[Content_Types].xml", opts)?;
+        zip.write_all(xml.as_bytes())?;
+        Ok(())
+    }
+    
+    fn add_core_properties(&self, zip: &mut ZipWriter<Cursor<Vec<u8>>>, opts: FileOptions) -> Result<()> {
+        let author = self.author.as_deref().unwrap_or("Unknown");
+        let title = self.title.as_deref().unwrap_or("Untitled");
+        let xml = format!(
+            r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <dc:creator>{}</dc:creator>
+  <cp:lastModifiedBy>{}</cp:lastModifiedBy>
+  <dc:title>{}</dc:title>
+  <dcmitype:imeType>application/vnd.openxmlformats-officedocument.presentationml.presentation</dcmitype:imeType>
+</cp:coreProperties>"#,
+            author, author, title
+        );
+        zip.start_file("_docProps/core.xml", opts)?;
+        zip.write_all(xml.as_bytes())?;
+        Ok(())
+    }
+    
+    fn add_app_properties(&self, zip: &mut ZipWriter<Cursor<Vec<u8>>>, opts: FileOptions) -> Result<()> {
+        let xml = format!(
+            r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+  <TotalTime>0</TotalTime>
+  <Slides>{}</Slides>
+  <Notes>{}</Notes>
+  <HiddenSlides>0</HiddenSlides>
+  <MMClips>0</MMClips>
+  <ScaleCrop>0</ScaleCrop>
+  <HeadingPairs>
+    <vt:vector size="2" baseType="variant">
+      <vt:variant><vt:lpstr>Presentation Name</vt:lpstr></vt:variant>
+      <vt:variant><vt:bstr>{}</vt:bstr></vt:variant>
+    </vt:vector>
+  </HeadingPairs>
+  <TitlesOfParts>
+    <vt:vector size="1" baseType="lpstr">
+      <vt:lpstr>{}</vt:lpstr>
+    </vt:vector>
+  </TitlesOfParts>
+  <Company/>
+  <Manager/>
+</Properties>"#,
+            self.slides.len(),
+            self.slides.len(),
+            self.title.as_deref().unwrap_or("Untitled"),
+            self.title.as_deref().unwrap_or("Untitled")
+        );
+        zip.start_file("_docProps/app.xml", opts)?;
+        zip.write_all(xml.as_bytes())?;
+        Ok(())
     }
 }
 
--- crates/apchat-pptx/tests/basic_presentation.rs
+++ crates/apchat-pptx/tests/basic_presentation.rs
@@ -24,3 +24,21 @@ fn test_add_content_slide() {
     ppt.add_content_slide("Slide Title", vec!["Bullet 1", "Bullet 2"]);
     assert_eq!(ppt.slides_count(), 1);
 }
+
+#[test]
+fn test_save_presentation() {
+    let mut ppt = Presentation::new()
+        .title("Test")
+        .author("Test Author");
+    
+    ppt.add_title_slide("Title", "Subtitle");
+    ppt.add_content_slide("Content", vec!["Bullet 1", "Bullet 2"]);
+    
+    let path = "/tmp/test_presentation.pptx";
+    ppt.save(path).unwrap();
+    
+    assert!(std::fs::metadata(path).is_ok());
+    
+    // Cleanup
+    std::fs::remove_file(path).ok();
+}
